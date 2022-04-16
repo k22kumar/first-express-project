56813e79c1e276b744a9949cec1b5a76c6723292
Kajanth <kajanthkumar@gmail.com>
2022-04-16 06:51:49 -0400

    send an image sample

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,11 @@
-const res = require("express/lib/response");
+// handles different ways operating systems handle paths to files
+const path = require('path');
 // using named functions with the function keyword allows node to tell us what function went wrong in the log when debugging. Node cant tell the name of the function if an arrow function is used. Good for debugging
 function getMessages(req, res) {
-    res.send('sample message')
+    // res.send('sample message');
+    // this needs to be an absolute path from the root of your system
+    // __dirname builtin variable to get the name of the folder where the current file lives, in this case the controllers folder
+    res.sendFile(path.join(__dirname, '..', 'public', 'KirbyFaceForward.png'));
 };
 
 function postMessage(req, res) {
